e53116b74f6eeaf35c2eb30d87252fc6ec5e466d
tashisrael <tashisrael04@gmail.com>
2024-11-06 11:39:38 +0300

    changes

--- index.js
+++ index.js
@@ -36,3 +36,16 @@ const headerLogoConatiner = document.querySelector('.header__logo-container')
 headerLogoConatiner.addEventListener('click', () => {
   location.href = 'index.html'
 })
+
+// Swiper.js initialization
+const swiper = new Swiper('.swiper-container', {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
